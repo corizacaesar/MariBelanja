042a1d25e30d42a607e65ec7ffa25ca9ff655bea
Kusmahendra <joker99isme@gmail.com>
2022-04-27 13:24:20 +0700

    Model

---
+++ MariBelanja/KatalogProduk/Models/Produk.cs
@@ -0,0 +1,13 @@
+﻿namespace KatalogProduk.Models
+{
+    public class Produk
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductCategory { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductPrice { get; set; }
+        //public int ProductStock { get; set; }
+        //public List<Shopping> Shopping { get; set; }
+    }
+}
