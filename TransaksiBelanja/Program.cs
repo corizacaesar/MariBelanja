eee40d2a916fc00b10277f1b4380ed1c8ccf97e8
Kusmahendra <joker99isme@gmail.com>
2022-04-29 06:59:55 +0700

    Program.cs maper and ef core

--- TransaksiBelanja/Program.cs
+++ TransaksiBelanja/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.EntityFrameworkCore;
 using TransaksiBelanja.AsyncDataService;
+using TransaksiBelanja.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//maper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+//EF Core
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("MyConnection")).EnableSensitiveDataLogging());
+
+//Maping
+builder.Services.AddScoped<IShopping, ShoppingRepo>();
+
 
 builder.Services.AddSingleton<IMessageAsyncClient, MessageAsyncClient>();
 
