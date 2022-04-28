4ebf89e87dd787844a33e3078fa27b1d9eb77be9
Suryani Rachmawati <81001726+suryanirachma@users.noreply.github.com>
2022-04-29 01:14:09 +0700

    Add files via upload
    membuat ProdukCreateDTO

---
+++ KatalogProduk/DTO/ProdukCreateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KatalogProduk.DTO
+{
+    public class ProdukCreateDTO
+    {
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public string ProductCategory { get; set; }
+        [Required]
+        public string ProductDescription { get; set; }
+        [Required]
+        public int ProductPrice { get; set; }
+    }
+}
