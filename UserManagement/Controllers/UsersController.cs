994803854b1344d82dab56d3375bc31f784e8c1a
Kusmahendra <39040686+Kusmahendra@users.noreply.github.com>
2022-04-29 21:46:59 +0700

    User Management

---
+++ UserManagement/Controllers/UsersController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UserManagement.Data;
+using UserManagement.Data.Dtos;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUser _users;
+
+        public UsersController(IUser users)
+        {
+            _users = users;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<ActionResult> Registration(UserCreateDto user)
+        {
+            try
+            {
+                await _users.Registration(user);
+                return Ok($"Registrasi User {user.UserName} berhasil");
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<UserViewDto> GetAllUsers()
+        {
+            return _users.GetAllUsers();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserViewDto>> Authenticate(UserCreateDto createUser)
+        {
+            try
+            {
+                var user = await _users.Authenticate(createUser.UserName, createUser.Password);
+                if (user == null) return NotFound();
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ UserManagement/Data/UserDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserManagement.Data
+{
+    public class UserDbContext : IdentityDbContext
+    {
+        public UserDbContext()
+        {
+
+        }
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=UserManagementDB")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name },
+                Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+    }
+}
